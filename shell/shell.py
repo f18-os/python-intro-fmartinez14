d47cb6cb85dcfc8a3dd7d4127ced8df1f9f42f57
Fernando Martinez <fmartinez14@miners.utep.edu>
2018-09-15 16:15:54 -0600

    working piping. Only missing background check.

--- shell/shell.py
+++ shell/shell.py
@@ -1,16 +1,27 @@
 #! /usr/bin/env python3
 
-import os, sys, time, re
-
+import os, sys, time, re , subprocess
 
 pid = os.getpid()
+ExtraPipe = []
 
 while(1): #Execute until Control C or exit command.
     try:
-        instruction = input("$") #Obtain command to run.
+
+
+        instruction = input(os.getcwd() + "$ ") #Obtain command to run.
         UserInput = instruction.split(" ")
 
+
+
+
+        if "cd" in UserInput:
+            getIndex = UserInput.index('cd')
+            os.chdir(UserInput[getIndex + 1])
+
+
         rc = os.fork()
+
 
         if rc < 0:
             os.write(2, ("fork failed, returning %d\n" % rc).encode())
@@ -24,19 +35,32 @@ while(1): #Execute until Control C or exit command.
             for dir in re.split(":", os.environ['PATH']): # try each directory in the path
                 program = "%s/%s" % (dir, UserInput[0])
                 try:
-                    if '<' in UserInput:
-                        getIndex = UserInput.index('<')
+                    if '>' in UserInput:
+                        getIndex = UserInput.index('>')
                         open(UserInput[getIndex+1],"w+") #Open file.
                         writeFile= os.open(UserInput[getIndex+1],os.O_WRONLY) #According to python documentation, this sets the write only flag.
                         os.dup2(writeFile,1) #Duplicate the file
                         UserInput= UserInput[:getIndex] #Overwrite the parameters with just what is required to execute.
-                    if '>' in UserInput:
-                        getIndex = UserInput.index('>') #Look for the pipe, if the file is found, execute it.
+                    if '<' in UserInput:
+                        getIndex = UserInput.index('<') #Look for the pipe, if the file is found, execute it.
                         newParams = open(UserInput[getIndex+1],"r").read().strip()
                         command = UserInput[0];
                         UserInput.clear() #Format new parameters to fit the syntax.
                         UserInput.append(command)
                         UserInput= UserInput+ newParams.split(' ')
+
+                    if '|' in UserInput:
+                        getIndex = UserInput.index('|')
+                        extraParams = UserInput[:getIndex]
+                        UserInput = UserInput[getIndex+1:]
+                        extraPipeCommand = subprocess.Popen(extraParams,stdout=subprocess.PIPE)
+                        addMe= extraPipeCommand.communicate()
+                        addMe = addMe[0].decode("utf-8").strip()
+                        UserInput.append(addMe)
+                        print(str(UserInput))
+
+                    if len(UserInput) < 1 or UserInput[0] == "":
+                        sys.exit(0)
 
                     os.execve(program, UserInput, os.environ) # try to exec program
                 except FileNotFoundError:             # ...expected
@@ -50,10 +74,10 @@ while(1): #Execute until Control C or exit command.
         else:                           # parent (forked ok)
             os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
                          (pid, rc)).encode())
-            UserInput = instruction.split(" ")
             childPidCode = os.wait() #Wait until child dies.
             os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
                          childPidCode).encode())
     except EOFError:
+        print("")
         sys.exit(0)
 sys.exit(0) #Kill process if control c or exit.
